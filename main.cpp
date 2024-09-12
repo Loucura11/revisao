14b8ae32c4c2831305a151b27c2ff3ee8db47df6
Loucura11 <gustavovicente01@gmail.com>
2024-09-12 00:49:04 +0000

    set_get

--- main.cpp
+++ main.cpp
@@ -1,5 +1,16 @@
+#include "Pessoa.cpp"
 #include<iostream>
+using namespace std;
+
 
 int main() {
- std::cout << "hello world\n";
+ Pessoa p;
+ p.setNome("Nome");
+ p.setSobrenome("Sobrenome");
+ p.setCPF(123456789);
+
+ cout<< "dados da pessoa" << endl;
+ cout << "Nome: " << p.getNome() << " " << p.getSobrenome() << endl;
+ cout << "CPF: " << p.getCPF() << endl;
+ 
 }
